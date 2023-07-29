7e4e07d20c8bef893c274c8b4f55ecd263bdc2d7
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-07-29 20:24:15 +0300

    Use zig as compiler
    Seems like this fixes my problems with the treesitter parsers not being
    loaded properly :shrug:

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,10 @@ return {
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
+    config = function(_, opts)
+      require("nvim-treesitter.install").compilers = { "zig" }
+      require("nvim-treesitter.configs").setup(opts)
+    end,
     opts = function(_, opts)
       -- add tsx and treesitter
       vim.list_extend(opts.ensure_installed, {
