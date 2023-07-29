ae3c559c5692dc5932bde06134e3e8bffc930cf0
Ilkka Poutanen <ilkka@ilkka.dev>
2023-07-29 11:45:30 +0300

    extras and stuff

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+return {
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "tsx",
+        "typescript",
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "go",
+        "elixir",
+        "rust",
+        "dockerfile",
+      })
+    end,
+  },
+}
