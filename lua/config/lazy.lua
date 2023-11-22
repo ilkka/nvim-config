0dad535dd688bcb50d843fa3cc7810fddb2ef68c
Ilkka Poutanen <ilkka@ilkka.dev>
2023-11-22 09:52:22 +0200

    Drop leap in favor of flash

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,7 +14,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.editor.leap" },
+    --{ import = "lazyvim.plugins.extras.editor.leap" },
     { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
