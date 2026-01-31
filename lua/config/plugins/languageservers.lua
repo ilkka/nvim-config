88eda30e9f0a6adcad11224fbd1e24cc72a88662
Ilkka Poutanen <ilkka@ilkka.dev>
2026-01-31 10:34:08 +0200

    clang and cmake lsps

--- lua/config/plugins/languageservers.lua
+++ lua/config/plugins/languageservers.lua
@@ -5,4 +5,6 @@ vim.lsp.enable("helm_ls")
 vim.lsp.enable("marksman")
 vim.lsp.enable("gradle_ls")
 vim.lsp.enable("golangci_lint_ls")
+vim.lsp.enable("clangd")
+vim.lsp.enable("cmake")
 -- NO SCALA HERE. I'm doing that with nvim-metals.
