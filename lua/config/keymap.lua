67d6c8047437393b22fe531f8e8b3c5886633d32
Ilkka Poutanen <ilkka.poutanen@61n.fi>
2026-02-06 10:56:02 +0200

    Alt keys movement

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -2,7 +2,9 @@
 -- keymap
 --
 
+--
 -- neovim QoL
+--
 
 -- move lines up or down
 vim.keymap.set("v", "J", ":move '>+1<cr>gv=gv", { desc = "Move selected line(s) down", silent = true })
@@ -15,6 +17,21 @@ vim.keymap.set({"n"}, "zS", function()
     local enable = vim.opt_local.scrolloff:get() == 0
     vim.opt_local.scrolloff = enable and 8 or 0
 end, { desc = "Toggle scrolloff" })
+-- exit terminal mode on esc
+vim.keymap.set({"t"}, "<esc>", "<c-\\><c-n>", { desc = "Exit terminal mode" })
+-- move with alt keys
+vim.keymap.set("t", "<A-h>", "<C-\\><C-N><C-w>h")
+vim.keymap.set("t", "<A-j>", "<C-\\><C-N><C-w>j")
+vim.keymap.set("t", "<A-k>", "<C-\\><C-N><C-w>k")
+vim.keymap.set("t", "<A-l>", "<C-\\><C-N><C-w>l")
+vim.keymap.set("i", "<A-h>", "<C-\\><C-N><C-w>h")
+vim.keymap.set("i", "<A-j>", "<C-\\><C-N><C-w>j")
+vim.keymap.set("i", "<A-k>", "<C-\\><C-N><C-w>k")
+vim.keymap.set("i", "<A-l>", "<C-\\><C-N><C-w>l")
+vim.keymap.set("n", "<A-h>", "<C-w>h")
+vim.keymap.set("n", "<A-j>", "<C-w>j")
+vim.keymap.set("n", "<A-k>", "<C-w>k")
+vim.keymap.set("n", "<A-l>", "<C-w>l")
 
 -- flash
 vim.keymap.set({ "n", "x", "o" }, 's', function() require("flash").jump() end, { desc = "Flash" })
