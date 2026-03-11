b9da21814348df0b410d1a2c9f51cbab7671be74
Ilkka Poutanen <ilkka.poutanen@61n.fi>
2026-03-11 10:27:31 +0200

    Turn on extra columns in oil

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -6,9 +6,9 @@ require("oil").setup({
   -- See :help oil-columns
   columns = {
     "icon",
-    -- "permissions",
-    -- "size",
-    -- "mtime",
+    "permissions",
+    "size",
+    "mtime",
   },
   -- Buffer-local options to use for oil buffers
   buf_options = {
